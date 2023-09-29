1ad78f982eaece8a578239e1fee9588d5628503d
V. Shivaramakrishnan <vsrkrishnan.1987@gmail.com>
2023-09-29 11:23:31 +0530

    Adding setup and teardown scripts

---
+++ teardown.sh
@@ -0,0 +1,25 @@
+
+################################################################################
+# Author: Viswanathan, Shivaramakrishnan
+# Email: shv@paloaltonetworks.com
+# Code Description:
+#   This script helps to automate the process of destroying all the resources that
+#   were deployed by running the setup script. This script requires full 
+#   read-write-execute access. If you run into permission issues, please 
+#   run the below command to update the permissions on the script,
+#       chmod 755 teardown.sh
+#   
+#   This script also requires terraform to be installed, version >= 0.15.3, < 2.0
+#   is preferred.
+#
+#   To run this script, simple run the below command in the same directory 
+#   where the script is located,
+#       ./teardown.sh
+#
+################################################################################
+
+cd $(dirname $0)
+
+terraform destroy -auto-approve
+
+echo "All resources for this deployment have been deleted."
